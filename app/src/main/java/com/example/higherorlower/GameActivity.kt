26c416d2736c25f15d20f4d89d19274b526553c2
Elin <sajkligen@ElinSimonmacair>
2022-09-28 08:51:18 +0200

    added class Deck

--- app/src/main/java/com/example/higherorlower/GameActivity.kt
+++ app/src/main/java/com/example/higherorlower/GameActivity.kt
@@ -1,5 +1,6 @@
 package com.example.higherorlower
 
+import Deck
 import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
@@ -22,9 +23,11 @@ class GameActivity : AppCompatActivity() {
     var difficulty: String? = null
     var life = 0
     var point = 0
-    var deck = mutableListOf<Card>()
+    //var deck = mutableListOf<Card>()
     var previousCard: Card? = null
     var currentCard: Card? = null
+    val deck = Deck()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,7 +42,7 @@ class GameActivity : AppCompatActivity() {
         layout.setBackgroundResource(R.drawable.greenbackground)
         difficulty = getDifficultyUser()
 
-        createDeck()
+        //createDeck()
         createLive(difficulty!!)
         lifeLeftTextView.text = getString(R.string.life_textView, life)
 
@@ -63,113 +66,6 @@ class GameActivity : AppCompatActivity() {
         pointTextView.text = getString(R.string.point_textview, 0)
     }
 
-    fun createDeck() {
-        val card1 = Card(R.drawable.heartace, 1)
-        val card2 = Card(R.drawable.heart2, 2)
-        val card3 = Card(R.drawable.heart3, 3)
-        val card4 = Card(R.drawable.heart4, 4)
-        val card5 = Card(R.drawable.heart5, 5)
-        val card6 = Card(R.drawable.heart6, 6)
-        val card7 = Card(R.drawable.heart7, 7)
-        val card8 = Card(R.drawable.heart8, 8)
-        val card9 = Card(R.drawable.heart9, 9)
-        val card10 = Card(R.drawable.heart10, 10)
-        val card11 = Card(R.drawable.heartj, 11)
-        val card12 = Card(R.drawable.heartq, 12)
-        val card13 = Card(R.drawable.heartk, 13)
-        val card14 = Card(R.drawable.clubace, 1)
-        val card15 = Card(R.drawable.club2, 2)
-        val card16 = Card(R.drawable.club3, 3)
-        val card17 = Card(R.drawable.club4, 4)
-        val card18 = Card(R.drawable.club5, 5)
-        val card19 = Card(R.drawable.club6, 6)
-        val card20 = Card(R.drawable.club7, 7)
-        val card21 = Card(R.drawable.club8, 8)
-        val card22 = Card(R.drawable.club9, 9)
-        val card23 = Card(R.drawable.club10, 10)
-        val card24 = Card(R.drawable.clubj, 11)
-        val card25 = Card(R.drawable.clubq, 12)
-        val card26 = Card(R.drawable.clubk, 13)
-        val card27 = Card(R.drawable.spadesace, 1)
-        val card28 = Card(R.drawable.spades2, 2)
-        val card29 = Card(R.drawable.spades3, 3)
-        val card30 = Card(R.drawable.spades4, 4)
-        val card31 = Card(R.drawable.spades5, 5)
-        val card32 = Card(R.drawable.spades6, 6)
-        val card33 = Card(R.drawable.spades7, 7)
-        val card34 = Card(R.drawable.spades8, 8)
-        val card35 = Card(R.drawable.spades9, 9)
-        val card36 = Card(R.drawable.spades10, 10)
-        val card37 = Card(R.drawable.spadesj, 11)
-        val card38 = Card(R.drawable.spadesq, 12)
-        val card39 = Card(R.drawable.spadesk, 13)
-        val card40 = Card(R.drawable.diamondsace, 1)
-        val card41 = Card(R.drawable.diamonds2, 2)
-        val card42 = Card(R.drawable.diamonds3, 3)
-        val card43 = Card(R.drawable.diamonds4, 4)
-        val card44 = Card(R.drawable.diamonds5, 5)
-        val card45 = Card(R.drawable.diamonds6, 6)
-        val card46 = Card(R.drawable.diamonds7, 7)
-        val card47 = Card(R.drawable.diamonds8, 8)
-        val card48 = Card(R.drawable.diamonds9, 9)
-        val card49 = Card(R.drawable.diamonds10, 10)
-        val card50 = Card(R.drawable.diamondsj, 11)
-        val card51 = Card(R.drawable.diamondsq, 12)
-        val card52 = Card(R.drawable.diamondsk, 13)
-        deck.add(card1)
-        deck.add(card2)
-        deck.add(card3)
-        deck.add(card4)
-        deck.add(card5)
-        deck.add(card6)
-        deck.add(card7)
-        deck.add(card8)
-        deck.add(card9)
-        deck.add(card10)
-        deck.add(card11)
-        deck.add(card12)
-        deck.add(card13)
-        deck.add(card14)
-        deck.add(card15)
-        deck.add(card16)
-        deck.add(card17)
-        deck.add(card18)
-        deck.add(card19)
-        deck.add(card20)
-        deck.add(card21)
-        deck.add(card22)
-        deck.add(card23)
-        deck.add(card24)
-        deck.add(card25)
-        deck.add(card26)
-        deck.add(card27)
-        deck.add(card28)
-        deck.add(card29)
-        deck.add(card30)
-        deck.add(card31)
-        deck.add(card32)
-        deck.add(card33)
-        deck.add(card34)
-        deck.add(card35)
-        deck.add(card36)
-        deck.add(card37)
-        deck.add(card38)
-        deck.add(card39)
-        deck.add(card40)
-        deck.add(card41)
-        deck.add(card42)
-        deck.add(card43)
-        deck.add(card44)
-        deck.add(card45)
-        deck.add(card46)
-        deck.add(card47)
-        deck.add(card48)
-        deck.add(card49)
-        deck.add(card50)
-        deck.add(card51)
-        deck.add(card52)
-    }
-
     fun handleHigherButtonPress() {
         randomCurrentCard()
         val checkGuess = checkGuessHigher()
@@ -185,17 +81,14 @@ class GameActivity : AppCompatActivity() {
     }
 
     fun randomPreviousCard() {
-        deck.shuffle()
-        previousCard = deck[10]
+        previousCard = deck.randomPreviousCard()
         imageViewPreviousCard.setImageResource(previousCard!!.image)
     }
 
     fun randomCurrentCard() {
-        deck.shuffle()
-        currentCard = deck[20]
+        currentCard = deck.randomNewCard()
         while (currentCard?.value == previousCard?.value) {
-            deck.shuffle()
-            currentCard = deck[25]
+           currentCard = deck.randomNewCard() // blanda igen
         }
         imageViewCurrentCard.setImageResource(currentCard!!.image)
     }
